e1ec049ea6370f702f4276640371c6dde2022407
db <191562687@qq.com>
2015-11-23 18:08:22 +0800

    初始化

---
+++ BothTech/BothTech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BothTech
+//
+//  Created by lanhull on 15/11/23.
+//  Copyright © 2015年 bothtech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
